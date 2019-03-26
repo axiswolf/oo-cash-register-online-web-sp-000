28f310d05ea3ca9ca869e6c75912d302db369b7a
Karen V <karenvo@usc.edu>
2019-03-26 11:08:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,7 @@ class CashRegister
         return "There is no discount to apply."
       end
       @total -= @total * @discount/100
-        return "After the discount, the total comes to $#{@total}"
+      return "After the discount, the total comes to $#{@total}"
   end
   def items
     # returns an array containing all items that have been added
