f78a0946ed3e7677f0eb937198ccc66ce37e3583
Karen V <karenvo@usc.edu>
2019-03-26 10:39:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,28 +1,27 @@
 class CashRegister
   attr_accessor :new
-
   def initialize(total)
     @total = 0
     # new sets an instance variable @total on initialization to zero
     # new optionally takes an employee discount on initalization
   end
-  def total(total)
+  def self.total(total)
     # returns current total
     @total = total
   end
-  def add_item(title,price,quantity=0)
+  def self.add_item(title,price,quantity=0)
     # accepts a title and a price and increase the total
     # also accepts an optional quantity
     # doesn't forget about the previous total
     previous_total = total
   end
-  def apply_discount()
+  def self.apply_discount()
     # Cash register was initalized with an employee discount applies the discount to total price
   end
-  def items
+  def self.items
     # returns an array containing all items that have been added
   end
-  def void_last_transaction
+  def self.void_last_transaction
     # subtracts the last transaction from the total
   end
 end
