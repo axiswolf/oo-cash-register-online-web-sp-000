471812dfd40debbfd3284d868aa3717ff129c1f9
Karen V <karenvo@usc.edu>
2019-03-26 10:41:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   attr_accessor :new
-  def initialize(new)
+  def initialize(new="employee_discount")
     @total = 0
     # new sets an instance variable @total on initialization to zero
     # new optionally takes an employee discount on initalization
